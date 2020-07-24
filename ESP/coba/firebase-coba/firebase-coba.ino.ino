93da65b587ecafab1667069368d7f94fca5af412
Gidi <nimen240@gmail.com>
2020-07-24 21:56:10 +0700

    Firebase connection testing

---
+++ ESP/coba/firebase-coba/firebase-coba.ino.ino
@@ -0,0 +1,38 @@
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+
+#define FIREBASE_HOST "nodemcu-test-6a3fc.firebaseio.com"
+#define FIREBASE_AUTH "1gFcr16y3dI3CHinRhshjLDLUoZpUTBkUhuPmX0c"
+ 
+#define WIFI_SSID "limanto"
+#define WIFI_PASS "220422101509"
+
+FirebaseData RFID;
+
+FirehaseJson json;
+
+void printResult(RFID &data);
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("\n connecting ");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected : ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
